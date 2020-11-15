b8e9d2654e9d4668f1a9c24535e38f962a3c219f
Indra Wahjoedi <indra.wahjoedi@gmail.com>
2020-11-15 14:15:22 +0700

    msg-> add init script

--- init-git-ftp.sh
+++ init-git-ftp.sh
@@ -1,37 +1,37 @@
 #!/bin/sh
 
-echo "[IW] git-ftp init start."
-git checkout -b git-ftp-init
-mv .gitignore gitignore
+echo "[IW] git-ftp init start." &&
+git checkout -b git-ftp-init &&
+mv .gitignore gitignore &&
 
 # Add your git-ftp exclude files here
-echo "docker-compose.yml" >> .gitignore
-echo "phpunit.xml.dist" >> .gitignore
-echo "spark" >> .gitignore
-echo ".git/*" >> .gitignore
-echo ".circleci/*" >> .gitignore
-echo ".editorconfig" >> .gitignore
-echo ".env.dev" >> .gitignore
-echo ".gitlab-ci.yaml" >> .gitignore
-echo "composer.json" >> .gitignore
-echo "composer.lock" >> .gitignore
-echo "docker-compose.yml" >> .gitignore
-echo "env.prod" >> .gitignore
-echo "README.md" >> .gitignore
+echo "docker-compose.yml" >> .gitignore &&
+echo "phpunit.xml.dist" >> .gitignore &&
+echo "spark" >> .gitignore &&
+echo ".git/*" >> .gitignore &&
+echo ".circleci/*" >> .gitignore &&
+echo ".editorconfig" >> .gitignore &&
+echo ".env.dev" >> .gitignore &&
+echo ".gitlab-ci.yaml" >> .gitignore &&
+echo "composer.json" >> .gitignore &&
+echo "composer.lock" >> .gitignore &&
+echo "docker-compose.yml" >> .gitignore &&
+echo "env.prod" >> .gitignore &&
+echo "README.md" >> .gitignore &&
 
-echo "app/*" >> .gitignore
-echo "system/*" >> .gitignore
-echo "writeable/*" >> .gitignore
-echo "vendor/*" >> .gitignore
+echo "app/*" >> .gitignore &&
+echo "system/*" >> .gitignore &&
+echo "writeable/*" >> .gitignore &&
+echo "vendor/*" >> .gitignore &&
 
-git add --all && git commit -m 'git-ftp-init' --quite
+git add --all && git commit -m 'git-ftp-init' --quite &&
 
-git rev-parse HEAD | xargs git checkout
-git ftp init -vv 
-git ftp catchup -vv
+git rev-parse HEAD | xargs git checkout &&
+git ftp init -vv  &&
+git ftp catchup -vv &&
 
-git checkout master
-git branch -d git-ftp-init
+git checkout master &&
+git branch -d git-ftp-init &&
 
 echo "[IW] git-ftp init done."
 
