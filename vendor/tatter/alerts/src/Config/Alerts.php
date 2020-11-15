1ade05dfa9ca82bdd47d99f987e19534f5ffa043
iwahjoedi <indra.wahjoedi@gmail.com>
2020-11-15 19:57:10 +0000

    build by circleci

---
+++ vendor/tatter/alerts/src/Config/Alerts.php
@@ -0,0 +1,15 @@
+<?php namespace Tatter\Alerts\Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class Alerts extends BaseConfig
+{
+	// prefix for SESSION variables and HTML classes, to prevent collision
+	public $prefix = "alerts-";	
+
+	// Template to use for HTML output
+	public $template = "Tatter\Alerts\Views\bootstrap";
+
+	// Whether to check session flashdata for common alert keys
+	public $getflash = true;
+}
---
+++ vendor/tatter/alerts/src/Exceptions/AlertsException.php
@@ -0,0 +1,16 @@
+<?php namespace Tatter\Alerts\Exceptions;
+
+use CodeIgniter\Exceptions\ExceptionInterface;
+use CodeIgniter\Exceptions\FrameworkException;
+
+class AlertsException extends FrameworkException implements ExceptionInterface
+{
+	public static function forInvalidTemplate(string $template = null)
+	{
+		return new static(lang('Alerts.invalidTemplate', [$template]));
+	}
+	public static function forMissingTemplateView(string $template = null)
+	{
+		return new static(lang('Alerts.missingTemplateView', [$template]));
+	}
+}
